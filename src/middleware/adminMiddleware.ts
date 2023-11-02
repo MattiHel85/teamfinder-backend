6b86e2060fa1cc1a14fb91e4b1cbcda332070963
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2023-11-02 14:08:32 +0200

    update isAdmin middleware

--- src/middleware/adminMiddleware.ts
+++ src/middleware/adminMiddleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { User } from "../types/user";
 
 export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
-    const user: User = req.user as User;
+    const user: User = req.body.user as User;
 
     if ( user && user.isAdmin){
         next();
